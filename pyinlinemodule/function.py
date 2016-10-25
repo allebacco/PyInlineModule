e0d5e5bf20a232d94c0c3abc421ac520a5722173
Alessandro Bacchini <allebacco@gmail.com>
2016-10-25 23:14:54 +0200

    Add Copyright

--- pyinlinemodule/function.py
+++ pyinlinemodule/function.py
@@ -1,3 +1,10 @@
+"""
+Copyright (c) 2016 Alessandro Bacchini <allebacco@gmail.com>
+
+All rights reserved. Use of this source code is governed by a
+MIT license that can be found in the LICENSE file.
+"""
+
 import inspect
 import dis
 import os
